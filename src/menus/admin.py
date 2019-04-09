2372f8975641828b6c7562bdb87bccbdd48547c4
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-09 17:27:43 +0200

    Added models: Menu, Meal & registered them in django admin

--- src/menus/admin.py
+++ src/menus/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from menus.models import Menu, Meal
+
+
+admin.site.register(Menu)
+admin.site.register(Meal)
--- src/menus/models.py
+++ src/menus/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model):
+    """Meal to be used in a menu"""
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    time_minutes = models.IntegerField()
+    is_vegan = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    """Menu object"""
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
