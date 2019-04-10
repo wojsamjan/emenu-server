695877133bb7e538836d8f34313afe270fdb993b
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-10 17:33:02 +0200

    Added MenuSerializer

--- src/menus/serializers.py
+++ src/menus/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from menus.models import Meal
+from menus.models import Meal, Menu
 
 
 class MealSerializer(serializers.ModelSerializer):
@@ -9,4 +9,18 @@ class MealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meal
         fields = ('id', 'name', 'description', 'price', 'time_minutes', 'is_vegan', 'created_date', 'modified_date')
+        read_only_fields = ('id', 'created_date', 'modified_date')
+        
+        
+class MenuSerializer(serializers.ModelSerializer):
+    """Serialize a menu"""
+    
+    meals = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Meal.objects.all()
+    )
+    
+    class Meta:
+        model = Menu
+        fields = ('id', 'name', 'description', 'created_date', 'modified_date', 'meals')
         read_only_fields = ('id', 'created_date', 'modified_date')
