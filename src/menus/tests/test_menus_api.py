16c88f0117988954ba7483d364f4addf5338c177
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-10 20:43:09 +0200

    Added tests for creating menus

--- src/menus/tests/test_menus_api.py
+++ src/menus/tests/test_menus_api.py
@@ -52,3 +52,55 @@ class MenusApiTests(TestCase):
 
         serializer = MenuDetailSerializer(menu)
         self.assertEqual(res.data, serializer.data)
+
+    def test_create_menu_successful(self):
+        """Test creating a new menu"""
+        payload = {
+            'name': 'Test menu name',
+            'description': 'Test menu description'
+        }
+        res = self.client.post(MENUS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        menu = Menu.objects.get(id=res.data['id'])
+        for key in payload.keys():
+            self.assertEqual(payload[key], getattr(menu, key))
+
+    def test_create_menu_invalid(self):
+        """Test creating a new menu with invalid payload"""
+        payload = {
+            'name': '',
+            'description': 'Test menu description'
+        }
+        res = self.client.post(MENUS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    def test_create_menu_not_unique(self):
+        """Test creating a new menu with not unique name"""
+        payload = {
+            'name': 'unique name',
+            'description': 'description'
+        }
+        self.client.post(MENUS_URL, payload)
+        res = self.client.post(MENUS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_menu_with_meals(self):
+        """Test creating a menu with meals"""
+        meal1 = create_meal(name='1st meal')
+        meal2 = create_meal(name='2nd meal')
+        payload = {
+            'name': 'Test menu name',
+            'description': 'Test menu description',
+            'meals': [meal1.id, meal2.id]
+        }
+        res = self.client.post(MENUS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        menu = Menu.objects.get(id=res.data['id'])
+        meals = menu.meals.all()
+        self.assertEqual(meals.count(), 2)
+        self.assertIn(meal1, meals)
+        self.assertIn(meal2, meals)
