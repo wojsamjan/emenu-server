5e8dd62dfb9d1c38ab1f93121004d50544748096
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-15 00:43:48 +0200

    Added fixtures/meals.json

--- emenu/settings.py
+++ emenu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+# Production SECRET_KEY is MODIFIED!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'pko(l#&vpto!4@ohmk7to$2f1kxalk^0ynx$od*!nshrtu!yq$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -149,7 +149,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 
 # Pagination
