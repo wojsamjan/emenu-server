5a8eb82d74de16a63c823337108a9c4d0a836644
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-17 00:07:48 +0200

    Added tests for image field, updated eMenu_coverage.png

--- menus/tests/test_models.py
+++ menus/tests/test_models.py
@@ -1,6 +1,9 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 from menus.models import Menu, Meal
+from menus import models
 
 
 def create_meal(name='meal', description='meal', price=1.99, time_minutes=30):
@@ -21,6 +24,16 @@ class MealTests(TestCase):
 
         self.assertTrue(isinstance(meal, Meal))
         self.assertEqual(str(meal), meal.name)
+
+    @patch('uuid.uuid4')
+    def test_meal_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.meal_image_file_path(None, 'image.jpg')
+    
+        exp_path = f'uploads/meal/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
 
 
 class MenuTests(TestCase):
